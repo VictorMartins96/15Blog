d5a94e8159d554f42da40d3cd817ec898b5e94d0
Victor Martins <victorgnr2010@hotmail.com>
2024-10-12 18:54:19 -0300

    Atualizando

--- contato.php
+++ contato.php
@@ -1,8 +1,7 @@
 <?php
     include_once("templates/header.php");
 ?>
-<body>
-    <h1>Olá mundo</h1>
+    <h1>Página de contato</h1>
 <?php
     include_once("templates/footer.php");
 ?>
---
+++ templates/footer.php
@@ -0,0 +1,3 @@
+<footer>Hora de Codar &copy; 2024</footer>
+</body>
+</html>
